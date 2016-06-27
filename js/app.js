8f5300f374d5db6206c7996e8dadb502226ed42f
Thomas Edstrom <thomasjedstrom@gmail.com>
2016-06-27 18:15:24 -0500

    generate world using javascript, pacman can now move, when he enters a square with a coin it disappears, there is a scoreboard now, it goes up by 10points when pacman eats a coin, and pacman cannot go through walls

--- js/app.js
+++ js/app.js
@@ -2,36 +2,94 @@
 //1.  have JS display the world of brick/coin/etc.      -   DONE!
 //2.  have the pacman move up, down, left and right
 
-    var world = [
-        [2,2,2,2,2,2,2,2,2,2],
-        [2,0,2,1,1,1,1,1,1,2],
-        [2,1,2,1,1,1,2,2,1,2],
-        [2,1,2,2,2,1,1,2,1,2],
-        [2,1,1,1,2,2,2,2,1,2],
-        [2,1,2,1,1,1,1,1,1,2],
-        [2,1,1,1,1,1,1,1,1,2],
-        [2,2,2,2,2,2,2,2,2,2]
-    ]
 
-    function displayWorld(){
-        var output = '';
+//WORLD
+var world = [
+    [2,2,2,2,2,2,2,2,2,2],
+    [2,0,2,1,1,1,1,1,1,2],
+    [2,1,2,1,1,1,2,2,1,2],
+    [2,1,2,2,2,1,1,2,1,2],
+    [2,1,1,1,2,2,2,2,1,2],
+    [2,1,2,1,1,1,1,1,1,2],
+    [2,1,1,1,1,1,1,1,1,2],
+    [2,2,2,2,2,2,2,2,2,2]
+]
 
-        for(var i=0; i<world.length; i++){
-            output += "\n<div class='row'>"
-            for(var j=0; j<world[i].length; j++){
-                if(world[i][j] == 2)
-                    output += "\n\t<div class='brick'></div>";
-                else if(world[i][j] == 1)
-                    output += "\n\t<div class='coin'></div>";
-                else if(world[i][j] == 0)
-                    output += "\n\t<div class='pacman'></div>";    
-                output = output + world[i][j];
-            }
-            output += "\n</div>"
+//PACMAN
+var pacman = {
+    x: 1,
+    y: 1
+}
+
+//SCORE
+var score = 0;
+
+//DISPLAY WORLD
+function displayWorld(){
+    var output = '';
+
+    for(var i=0; i<world.length; i++){
+        output += "\n<div class='row'>\n"
+        for(var j=0; j<world[i].length; j++){
+            if(world[i][j] == 2)
+                output += "<div class='brick'></div>";
+            else if(world[i][j] == 1)
+                output += "<div class='coin'></div>";
+            else if(world[i][j] == 0)
+                output += "<div class='empty'></div>";    
+            //output = output + world[i][j];
         }
-        $('#world').html(output);
+        output += "\n</div>"
     }
+    $('#world').html(output);
+}
+
+//DISPLAY PACMAN
+function displayPacman(){
+    document.getElementById('pacman').style.left = pacman.x*20+"px";
+    document.getElementById('pacman').style.top = pacman.y*20+"px";
+}
+//DISPLAY SCORE
+function displayScore(){
+    document.getElementById('score').innerHTML = score;
+}
+
+
+//PACMAN MOVEMENT
+document.onkeydown = function(e){
+//LEFT
+    if(e.keyCode == 37 && world[pacman.y][pacman.x-1]!=2){
+        pacman.x --;
+    }
+//RIGHT
+    else if(e.keyCode == 39 && world[pacman.y][pacman.x+1]!=2){
+        pacman.x ++;
+    }
+//DOWN
+    else if(e.keyCode == 38 && world[pacman.y-1][pacman.x]!=2){
+        pacman.y --;
+    }
+//UP
+    else if(e.keyCode == 40 && world[pacman.y+1][pacman.x]!=2){
+        pacman.y ++;
+    }
+    
+    if(world[pacman.y][pacman.x] == 1){
+        world[pacman.y][pacman.x] = 0;
+        score+=10;
+        displayWorld();
+        displayScore();
+    }
+    
+    
+    displayPacman()
+}
+
+
 
 $(document).ready(function(){
     displayWorld();
+    displayPacman();
+    displayScore();
+
 })
