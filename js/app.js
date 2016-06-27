8967aa3cecea490990c4a615e7cf02bb5b2b8835
Thomas Edstrom <thomasjedstrom@gmail.com>
2016-06-27 18:56:24 -0500

    pacman now rotates tot he direction he's moving.  added cherries worth 50pts.  added a ghost which can't move yet.  made maze a lot bigger.

--- js/app.js
+++ js/app.js
@@ -1,18 +1,20 @@
-//Features to build
-//1.  have JS display the world of brick/coin/etc.      -   DONE!
-//2.  have the pacman move up, down, left and right
-
-
 //WORLD
 var world = [
-    [2,2,2,2,2,2,2,2,2,2],
-    [2,0,2,1,1,1,1,1,1,2],
-    [2,1,2,1,1,1,2,2,1,2],
-    [2,1,2,2,2,1,1,2,1,2],
-    [2,1,1,1,2,2,2,2,1,2],
-    [2,1,2,1,1,1,1,1,1,2],
-    [2,1,1,1,1,1,1,1,1,2],
-    [2,2,2,2,2,2,2,2,2,2]
+    [2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2],
+    [2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2],
+    [2,1,2,2,2,2,2,2,2,1,2,1,2,2,2,2,2,2,2,1,2],
+    [2,1,2,1,1,1,1,1,1,1,2,1,1,1,1,1,1,1,2,1,2],
+    [2,1,2,4,2,2,2,2,2,1,2,1,2,2,2,2,2,4,2,1,2],
+    [2,1,2,1,1,1,1,1,1,1,2,1,1,1,1,1,1,1,2,1,2],
+    [2,1,2,2,2,2,2,2,2,1,2,1,2,2,2,2,2,2,2,1,2],
+    [2,1,1,1,1,1,1,1,1,1,2,1,1,1,1,1,1,1,1,1,2],
+    [2,1,2,2,2,2,2,2,2,1,2,1,2,2,2,2,2,2,2,1,2],
+    [2,1,2,1,1,1,1,1,1,1,2,1,1,1,1,1,1,1,2,1,2],
+    [2,1,2,4,2,2,2,2,2,1,2,1,2,2,2,2,2,4,2,1,2],
+    [2,1,2,1,1,1,1,1,1,1,2,1,1,1,1,1,1,1,2,1,2],
+    [2,1,2,2,2,2,2,2,2,1,2,1,2,2,2,2,2,2,2,1,2],
+    [2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,5,2],
+    [2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2]
 ]
 
 //PACMAN
@@ -27,11 +29,14 @@ var score = 0;
 //DISPLAY WORLD
 function displayWorld(){
     var output = '';
-
     for(var i=0; i<world.length; i++){
         output += "\n<div class='row'>\n"
         for(var j=0; j<world[i].length; j++){
-            if(world[i][j] == 2)
+            if(world[i][j] == 5)
+                output += "<div id='ghost'></div>";
+            else if(world[i][j] == 4)
+                output += "<div class='cherries'></div>";
+            else if(world[i][j] == 2)
                 output += "<div class='brick'></div>";
             else if(world[i][j] == 1)
                 output += "<div class='coin'></div>";
@@ -49,6 +54,7 @@ function displayPacman(){
     document.getElementById('pacman').style.left = pacman.x*20+"px";
     document.getElementById('pacman').style.top = pacman.y*20+"px";
 }
+
 //DISPLAY SCORE
 function displayScore(){
     document.getElementById('score').innerHTML = score;
@@ -59,24 +65,48 @@ function displayScore(){
 document.onkeydown = function(e){
 //LEFT
     if(e.keyCode == 37 && world[pacman.y][pacman.x-1]!=2){
+        $('#pacman').removeClass('right');
+        $('#pacman').removeClass('up');
+        $('#pacman').removeClass('down');
+        $('#pacman').addClass('left');
         pacman.x --;
     }
 //RIGHT
     else if(e.keyCode == 39 && world[pacman.y][pacman.x+1]!=2){
+        $('#pacman').removeClass('left');
+        $('#pacman').removeClass('up');
+        $('#pacman').removeClass('down');
+        $('#pacman').addClass('right');
         pacman.x ++;
     }
 //DOWN
     else if(e.keyCode == 38 && world[pacman.y-1][pacman.x]!=2){
+        $('#pacman').removeClass('right');
+        $('#pacman').removeClass('up');
+        $('#pacman').removeClass('left');
+        $('#pacman').addClass('down');
         pacman.y --;
     }
 //UP
     else if(e.keyCode == 40 && world[pacman.y+1][pacman.x]!=2){
+        $('#pacman').removeClass('right');
+        $('#pacman').removeClass('left');
+        $('#pacman').removeClass('down');
+        $('#pacman').addClass('up');
         pacman.y ++;
     }
     
+//PACMAN SCORING COINS
     if(world[pacman.y][pacman.x] == 1){
         world[pacman.y][pacman.x] = 0;
         score+=10;
+        displayWorld();
+        displayScore();
+    }
+//PACMAN SCORING CHERRIES
+    if(world[pacman.y][pacman.x] == 4){
+        world[pacman.y][pacman.x] = 0;
+        score+=50;
         displayWorld();
         displayScore();
     }
